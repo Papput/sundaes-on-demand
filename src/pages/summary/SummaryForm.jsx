319b02e79163fa01b948705bde60e00328fa9c3d
Gunnar Abrahamsson <gunnar.abrahamsson@gmail.com>
2021-05-28 13:17:58 +0200

    Tesing hover

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,14 +1,43 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Popover, OverlayTrigger } from 'react-bootstrap'
 
+
+// const popover = (
+//     <Popover id="popover-basic">
+//       <Popover.Header as="h3">Popover right</Popover.Header>
+//       <Popover.Body>
+//         And here's some <strong>amazing</strong> content. It's very engaging.
+//         right?
+//       </Popover.Body>
+//     </Popover>
+//   );
+  
+//   const Example = () => (
+//     <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+//       <Button variant="success">Click me to see</Button>
+//     </OverlayTrigger>
+//   );
 const SummaryForm = () => {
     const [checkBoxChecked, setCheckBoxChecked] = useState(false);
 
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Content>
+                No ice cream will actually be delivered
+            </Popover.Content>
+        </Popover>
+    );
+
     const checkboxLabel = (
         <span>
-            I agree to <span style={{ color: "blue" }}> Terms and Conditions</span>
+            I agree to 
+                <OverlayTrigger trigger={["hover", "focus"]} placement="right" overlay={popover}>
+                    <span style={{ color: "blue" }}> Terms and Conditions</span>
+                </OverlayTrigger>
+                
         </span>
-    )
+    );
+
     return (
         <Form>
             <Form.Group controlId="terms-and-conditions">
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
-
+import userEvent from '@testing-library/user-event';
 // checkbox is unchecked by default
 test('Checkbox & button initial state', () => {
     render(<SummaryForm />);
@@ -19,13 +19,32 @@ test('checkbox enables and disables button', () => {
     expect(confirmOrdersButton).toBeDisabled();
     
     // checkbox enables button
-    fireEvent.click(enableButtonCheckbox);
+    userEvent.click(enableButtonCheckbox);
     
     expect(confirmOrdersButton).toBeEnabled();
     
     // unchecking checkbox disables button 
-    fireEvent.click(enableButtonCheckbox);
+    userEvent.click(enableButtonCheckbox);
     
     expect(confirmOrdersButton).toBeDisabled();
 
+});
+
+test('popover responds to hover', async () => {
+    render(<SummaryForm />);
+
+    //popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+
+    expect(nullPopover).not.toBeInTheDocument();
+    // popover appers upon mouseover of checkbox label
+    const termsAndCondition = screen.getByText(/terms and conditions/i)
+    userEvent.hover(termsAndCondition);
+    
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    
+    // popover disappears when we mouse out
+    userEvent.unhover(termsAndCondition);
+    
+    await waitForElementToBeRemoved(popover);
 })
